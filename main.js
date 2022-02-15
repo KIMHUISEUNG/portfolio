d42a5d302292c2f1be3c826b4863d7d082db646d
hatso <mmorpg3636@naver.com>
2022-02-15 17:50:37 +0900

    JS: navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// navbar가 스크롤 하여 내려가면 투명하게 만든다.
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
